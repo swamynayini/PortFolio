334e4f2cfc8e07715c39c7ca58db8d23c9729c1d
Swamy Nayini <119121732+swamynayini@users.noreply.github.com>
2024-07-15 09:33:10 +0530

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom/cjs/react-router-dom.min";
 const Header = ()=>{
     return(
         <div>
-            <ul className=" p-4 m-4 flex justify-end space-x-4 cursor-pointer font-medium">
+            <ul className=" md:p-4 m-4 flex justify-end space-x-4 cursor-pointer font-medium">
                 <li><Link to ="/">Home</Link></li>
                 <li ><Link to="/about">About</Link></li>
                 <li><Link to ="/contact">Contact </Link></li>
