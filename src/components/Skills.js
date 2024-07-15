30f2219a2cb3756438214b4463345ab025847fb1
Swamy Nayini <119121732+swamynayini@users.noreply.github.com>
2024-07-15 09:38:58 +0530

    Update Skills.js

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -3,14 +3,14 @@ import { BOOTSTRAP_LOGO, CSS_LOGO, HTML_LOGO, JAVASCRIPT_LOGO, PYTHON_LOGO, REAC
 
 const Skills = () => {
   return (
-    <div className='mt-[-3.5rem]'>
-      <h3 className="ml-[34.5rem] text-3xl text-lime-400 ">
+    <div className='md:mt-[-3.5rem]'>
+      <h3 className="md:ml-[34.5rem] text-3xl text-lime-400 ">
         My Skillss*
       </h3>
-      <div className="container py-2 my-2 text-center ">
+      <div className="container md:py-2 md:my-2 text-center ">
         <div className="row align-items-center">
             <div className="row mx-3 align-items-center mb-2">
-            <img className='h-10 w-10 ml-[33.5rem] mb-[-2.5rem]' alt='logo' src={HTML_LOGO}/> 
+            <img className='md:h-10 w-10 md:ml-[33.5rem] mb-[-2.5rem]' alt='logo' src={HTML_LOGO}/> 
               <h4>HTML</h4>
               <h6 className="text-white mt-3">
                 <span>★★★★</span>★
@@ -18,14 +18,14 @@ const Skills = () => {
                
               </div>
               <div className="col-6 col-lg-4 py-3 px-3">
-              <img className='h-10 w-10 ml-[33.5rem] mb-[-2.5rem]' alt='logo' src={CSS_LOGO}/>
+              <img className='md:h-10 w-10 md:ml-[33.5rem] mb-[-2.5rem]' alt='logo' src={CSS_LOGO}/>
                 <h4>CSS</h4>
                 <h6 className="text-white mt-3">
                   <span>★★★</span>★★
                 </h6>  
               </div>
               <div className="col-6 col-lg-4 py-3 px-3">
-              <img className='h-10 w-10 ml-[33.5rem] mb-[-2.5rem]' alt='logo' src={JAVASCRIPT_LOGO}/>
+              <img className='md:h-10 w-10 md:ml-[33.5rem] mb-[-2.5rem]' alt='logo' src={JAVASCRIPT_LOGO}/>
                 <h4>JavaScript</h4>
                 <h6 className="text-white mt-3">
                   <span>★★★</span>★★
@@ -33,28 +33,28 @@ const Skills = () => {
                 
               </div>
               <div className="col-6 col-lg-4 py-3 px-3">
-              <img className='h-10 w-10 ml-[33.5rem] mb-[-2.5rem]' alt='logo' src={BOOTSTRAP_LOGO}/>
+              <img className='md:h-10 w-10 md:ml-[33.5rem] mb-[-2.5rem]' alt='logo' src={BOOTSTRAP_LOGO}/>
                 <h4>Bootstrap</h4>
                 <h6 className="text-white mt-3">
                   <span>★★★★</span>★
                 </h6>
               </div>
               <div className="col-6 col-lg-4 py-3 px-3">
-              <img className='h-10 w-10 ml-[33.5rem] mb-[-2.5rem]' alt='logo' src={PYTHON_LOGO}/>
+              <img className='md:h-10 w-10 md:ml-[33.5rem] mb-[-2.5rem]' alt='logo' src={PYTHON_LOGO}/>
                 <h4>Python</h4>
                   <h6 className="text-white mt-3">
                     <span>★★★</span>★★
                   </h6>
               </div>
               <div className="col-6 col-lg-4 py-3 px-3">
-              <img className='h-10 w-10 ml-[33.5rem] mb-[-2.5rem]' alt='logo' src={REACT_LOGO}/>
+              <img className='md:h-10 w-10 ml-[33.5rem] md:mb-[-2.5rem]' alt='logo' src={REACT_LOGO}/>
                 <h4>React js</h4>
                 <h6 className="text-white mt-3">
                   <span>★★★</span>★★
                 </h6>
               </div>
               <div className="col-6 col-lg-4 py-3 px-3">
-              <img className='h-10 w-10 ml-[33.5rem] mb-[-2.5rem]' alt='logo' src={SQL_LOGO}/>
+              <img className='md:h-10 w-10 md:ml-[33.5rem] mb-[-2.5rem]' alt='logo' src={SQL_LOGO}/>
                 <h4>SQL</h4>
                 <h6 className="text-white mt-3">
                   <span>★★★</span>★★
