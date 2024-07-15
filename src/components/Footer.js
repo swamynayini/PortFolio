2e13ea756e3213659ae919a9e0615a236ad42de8
Swamy Nayini <119121732+swamynayini@users.noreply.github.com>
2024-07-15 09:32:41 +0530

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Footer = () => {
   return (
     <footer className='text-center py-3'>
-        <p className='my-2 pt-4'>Copyright &copy; 2024 by Swamy Nayini | All Rights Reserved</p>
+        <p className='md:my-2 pt-4'>Copyright &copy; 2024 by Swamy Nayini | All Rights Reserved</p>
     </footer>
   )
 }
